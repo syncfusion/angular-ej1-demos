5d4a27e8a276cd578fa73b7ae81019e8dc1917b7
SyncfusionInstall <buildautomation@syncfusion.com>
2020-04-01 16:35:20 +0530

    v18.1.0.42

--- dist/app/components/treeview/nodeediting.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ej-app',
-     styleUrls: ['./treeview.component.css'],
-    templateUrl: './nodeediting.component.html'
-})
-export class NodeeditingComponent { }
