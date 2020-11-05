a0198387787176a75d949b1c50138a129d17565b
SyncfusionInstall <buildautomation@syncfusion.com>
2020-11-05 19:25:17 +0530

    v18.3.0.47

--- app/components/scroller/default.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ej-app',
-  templateUrl: './default.component.html'
-})
-export class DefaultComponent {
-  constructor() { }
-}
