4dc3b7e2c0405f4b935cc95cb36195d860510c87
buildautomation <buildautomation@syncfusion.com>
2020-12-17 17:52:27 +0530

    v18.4.0.30

--- app/components/treeview/checkbox.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ej-app',
-     styleUrls: ['./treeview.component.css'],
-    templateUrl: './checkbox.component.html'
-})
-export class CheckboxComponent { }
