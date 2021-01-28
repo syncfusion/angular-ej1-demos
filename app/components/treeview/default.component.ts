0717b10fb1d8f39334ac6b13e4ee497fe0b3f8ff
buildautomation <buildautomation@syncfusion.com>
2021-01-28 14:37:02 +0530

    v18.4.0.39

--- app/components/treeview/default.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ej-app',
-    templateUrl: './default.component.html',
-    styleUrls: ['./treeview.component.css'],
-})
-export class DefaultComponent { }
