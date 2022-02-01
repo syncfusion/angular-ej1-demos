a72d6a436a94b826864919f80fa0c1a0b5165e63
SyncfusionBuild <buildautomation@syncfusion.com>
2022-02-01 10:20:51 +0530

    v19.4.0.48

--- app/toolbar.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'sd-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
-})
-export class ToolbarComponent {}
--- dist/app/components/timepicker/default.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ej-app',
-  templateUrl: './default.component.html'
-})
-export class DefaultComponent {
-
-}
