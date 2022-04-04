f69fb9935d9ad29ba2572104bf1674b2328de0eb
SyncfusionBuild <buildautomation@syncfusion.com>
2022-04-04 15:27:13 +0530

    v20.1.0.47

--- app/components/treeview/icons.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ViewEncapsulation } from '@angular/core';
-
-@Component({
-    selector: 'ej-app',
-    templateUrl: './icons.component.html',
-    styleUrls: ['./icons.component.css', './treeview.component.css'], encapsulation: ViewEncapsulation.None,
-})
-export class IconsComponent { }
--- app/components/treeview/rtl.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ej-app',
-     styleUrls: ['./treeview.component.css'],
-    templateUrl: './rtl.component.html'
-})
-export class RTLComponent { }
